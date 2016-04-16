e6c893a3d6139732101ff0f7a3331b7007a1615f
ORT1 <ort1@ort1s-imac.local>
2016-04-16 14:47:16 -0300

    work in progress

---
+++ AppLista/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  AppLista
+//
+//  Created by ORT1 on 4/16/16.
+//  Copyright © 2016 ORT. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var nome: String
+    
+    init(nome: String){
+        self.nome = nome
+    }
+}
--- AppLista/ListaTableViewController.swift
+++ AppLista/ListaTableViewController.swift
@@ -10,43 +10,44 @@ import UIKit
 
 class ListaTableViewController: UITableViewController {
 
+    var items = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        items.appendContentsOf([Item(nome:"Buy Something"), Item(nome:"Sell something"), Item(nome:"Profit!")])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListaPrototypeCell", forIndexPath: indexPath)
 
+        let item = items[indexPath.row]
+
+        cell.textLabel!.text = item.nome
+        
         // Configure the cell...
 
         return cell
     }
-    */
 
+    @IBAction func voltaParaLista(segue: UIStoryboardSegue) {
+        // TODO
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
